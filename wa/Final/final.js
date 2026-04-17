fe72e81c9c154bc570d6a7a6d805923755bb4e0a
Laura-Jumper <laju9212@colorado.edu>
2026-04-16 23:39:12 -0600

    final

---
+++ wa/Final/final.js
@@ -0,0 +1,4 @@
+const gameArea = document.getElementById("gameArea");
+const targetDisplay = document.getElementById("target");
+let targetVolume = getRandomVolume();
+targetDisplay.textContent = targetVolume;
